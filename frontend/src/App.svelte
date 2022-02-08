02544d841702d3493295a49f1e84d54f6e4dbc9c
Mr. M1M3 <abir.sheikh.4044@gmail.com>
2022-02-08 12:54:10 +0600

    initial commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+// imports css files
+import "./tailwind.css"
+</script>
+<main>
+    <h1>Initail Commit of making JSON Hero</h1>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./frontend/src/**/*.svelte'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
