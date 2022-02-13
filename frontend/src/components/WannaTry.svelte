eceb5ccf3fae214e863d5ad30bc2199191ead335
Mr. M1M3 <abir.sheikh.4044@gmail.com>
2022-02-13 12:19:19 +0600

    fixed responsiveness issue in wannatry section

--- frontend/src/components/WannaTry.svelte
+++ frontend/src/components/WannaTry.svelte
@@ -3,7 +3,7 @@
     <h1 class="text-5xl text-center font-paytone mx-3 my-3">Want To Give It A Try?</h1>
     <article class="buttons">
         <button class="px-4 py-3  rounded-md bg-scheme-dark text-white font-bold font-andika">Create An Account</button>
-        <button class="px-4 py-3  rounded-md bg-scheme-dark text-white font-bold font-andika">Login WIth An Existing Account</button>
+        <button class="px-4 py-3  rounded-md bg-scheme-dark text-white font-bold font-andika">Login</button>
     </article>
 </section>
 <style lang="scss">
@@ -12,7 +12,7 @@
             padding: 0 1rem;
             display: grid;
             gap: 1rem;
-            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+            grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
         }
     }
 </style>
